abf09bd92b9f857a26a3468c15420af3f530f371
Tishan Murugesan <e237536@sbl.edu.ch>
2026-01-30 08:24:39 +0000

    Aufgabe: ex.06.js vervollständigt

--- src/exercises/ex.06.js
+++ src/exercises/ex.06.js
@@ -1,9 +1,10 @@
 const number = Number(process.argv[2]);
 
-const remainder = number % 3; // schaut wie viel rest es gibt bei einer divission dur 3
+const remainder = number % 3;
 
 if (remainder === 0) {
 	console.log("Die Zahl ist durch 3 teilbar.");
 } else {
 	console.log("Die Zahl ist nicht durch 3 teilbar.");
+	w;
 }
